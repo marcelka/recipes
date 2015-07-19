5701fd85389070258f92bbc4b6df00e53513cba1
Tomas Kulich <tomas.kulich@gmail.com>
2015-07-19 19:45:13 +0200

    vlux added

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,25 +1,29 @@
-import React, {Component, findDOMNode} from 'react';
+import React, {Component, findDOMNode, PropTypes} from 'react';
 import {Link} from 'react-router';
 
-export class Home extends  Component {
-  state = {
+export class Home extends Component {
+    state = {
     counter: 7,
     todos: [],
   };
 
   handleUpdate() {
-    this.setState({counter: this.state.counter +1});
+    this.setState({counter: this.state.counter + 1});
   }
 
   decrease() {
-    this.setState({counter: this.state.counter -1});
+    this.setState({counter: this.state.counter - 1});
   }
 
   addItem() {
     const input = findDOMNode(this.refs.test);
     const {value} = input;
     this.setState({todos: [...this.state.todos, value]});
-    input.value = "";
+    input.value = ''
+  }
+
+  static contextTypes = {
+    disp: React.PropTypes.object.isRequired,
   }
 
   render() {
@@ -31,9 +35,9 @@ export class Home extends  Component {
         <div> <Link to="add-item">Add item</Link> </div>
         <hr />
 
-        <div>{this.state.counter}</div>
-        <button onClick={::this.decrease}>-1</button>
-        <button onClick={::this.handleUpdate}>+1</button>
+        <div> {this.context.disp.state.num} </div>
+        <button onClick={() => this.context.disp.dispatch('dec', 1)}>-1</button>
+        <button onClick={() => this.context.disp.dispatch('inc', 1)}>+1</button>
         <hr />
         <div><input ref="test"/></div>
         <div><button onClick={::this.addItem}>Add item</button></div>
--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import {DefaultRoute, NotFoundRoute, Route} from 'react-router';
 
 import {App} from "./components/App"
 import {Home} from "./components/Home"
 import {AddItem} from "./components/AddItem"
 import {ComposeRecipe} from "./components/ComposeRecipe"
+import {Dispatcher} from 'vlux';
+
+const countingStore = (state, action, payload) => {
+  switch (action) {
+    case 'inc':
+      return {...state, num: state.num + payload}
+    case 'dec':
+      return {...state, num: state.num - payload}
+    default:
+      return state
+  }
+}
+
+
+var dispatcher = new Dispatcher(countingStore, {num: 0, todos:[]});
+
+class AppWrapper extends Component {
+
+  static childContextTypes = {
+    disp: React.PropTypes.object,
+  }
+
+  getChildContext() {
+    return {
+      disp: dispatcher,
+    };
+  }
+
+  componentDidMount() {
+    dispatcher.on('change', () => this.setState({}));
+  }
+
+  render(){
+    return (<App />);
+  }
+}
+
+dispatcher.on('change', ()=>{console.log('state change')});
 
 export const routes = (
-  <Route handler={App} path="/">
+  <Route handler={AppWrapper} path="/">
     <DefaultRoute handler={Home} name="home" />
     <Route handler={AddItem} name="add-item" path="add-item"/>
     <Route handler={ComposeRecipe} name="compose-recipe" path="compose-recipe" />
